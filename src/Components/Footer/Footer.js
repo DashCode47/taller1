92fc7d5b46119ba1e160444afdc4492875a66f67
David Lozada <davsh47@hotmail.com>
2022-11-26 01:47:45 +0300

    ageCalculator finished

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      © Kruger Corp. 2022. Transforming Enterprises, Boosting Lives.
+    </div>
+  );
+};
+
+export default Footer;
